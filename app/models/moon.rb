ed062f785e6598f7df1882e54277658213bc55d2
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-09-03 18:58:20 +0100

    hiding api key

--- app/models/moon.rb
+++ app/models/moon.rb
@@ -4,7 +4,7 @@ class Moon < ApplicationRecord
     require "json"
     require "open-uri"
 
-    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY"
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2022-09-03?key=#{api_key_visualcrossing}&include=days&elements=datetime,moonphase,sunrise,sunset,moonrise,moonset"
     user_serialized = URI.open(url).read
     user = JSON.parse(user_serialized)
   end
