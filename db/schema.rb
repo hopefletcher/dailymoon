439c61303af4cd03457c7ff31e04b039726c34e6
Hope Fletcher <thehopefletcher@gmail.com>
2022-09-03 15:13:55 +0200

    Migrated User db to include Admin :boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_03_092454) do
+ActiveRecord::Schema.define(version: 2022_09_03_131131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2022_09_03_092454) do
     t.string "location"
     t.date "birthday"
     t.string "zodiac_sign"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "faker"
+puts "Cleaning database..."
+
+User.destroy_all
+Mood.destroy_all
+Event.destroy_all
+Moon.destroy_all
+
+puts "Creating users..."
+fakeuser1 = User.create!(email: "user@mail.com", password: "123456", first_name: "fake", last_name: "user", location: "Barcelona, Spain", birthday: "1992-06-04", zodiac_sign: "gemini")
+fakeuser2 = User.create!(email: "user2@mail.com", password: "123456", first_name: "fake2", last_name: "user2", location: "Madrid, Spain", birthday: "1985-12-19", zodiac_sign: "sagittarius")
+fakeuser3 = User.create!(email: "user3@mail.com", password: "123456",  first_name: "fake3", last_name: "user3", location: "Lisbon, Portugal", birthday: "2002-09-01", zodiac_sign: "virgo")
+fakeuser3 = User.create!(email: "user4@mail.com", password: "123456",  first_name: "fake4", last_name: "user4", location: "Alameda, California", birthday: "1990-10-31", zodiac_sign: "scorpio")
+
+puts "Creating Moods..."
+# t.integer "rating"
+# t.text "journal_entry"
+# t.date "date"
+# t.bigint "user_id", null: false
+60.times do
+  mood = Mood.new(
+    rating: rand(1..5),
+    journal_entry: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    date: rand(0..5)
+  )
+  restaurant.save!
+end
+puts 'Finished!'
