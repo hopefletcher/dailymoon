c667221b18701d98f1af93d4b2a3774e331f0dd6
filipe dias <filipediasit@gmail.com>
2022-09-03 11:44:34 +0100

    added Routes and Controllers

---
+++ app/controllers/moods_controller.rb
@@ -0,0 +1,2 @@
+class MoodsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :moods, only: [:new, :create]
+  resources :events, only: [:new, :create]
+  get 'stats', to: 'pages#stats'
+  get 'month', to: 'calendar#month'
+  get 'day', to: 'calendar#day'
 end
