f7443e13167619bcfc2fa3b04dd3d8c13f620ae9
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-09-03 17:10:03 +0100

    added fetch_moon_data method to Moon model

--- app/models/moon.rb
+++ app/models/moon.rb
@@ -1,2 +1,11 @@
 class Moon < ApplicationRecord
+
+  def fetch_moon_data
+    require "json"
+    require "open-uri"
+
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Barcelona,ES?key=R597FYUYAA9JQJRLNDNTD4C6E"
+    user_serialized = URI.open(url).read
+    user = JSON.parse(user_serialized)
+  end
 end
