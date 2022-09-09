83b48b77a527908d9ba83de207b84a972867fe3f
George Chaves <george_hfc@yahoo.com.br>
2022-09-09 22:34:01 +0100

    rails-admin installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
