5a970e54392ca894e8c1660fbda4c4425d543510
George Chaves <george_hfc@yahoo.com.br>
2022-09-10 14:32:53 +0100

    navbar fix, killed pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'calendar#day'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :moods, only: [:new, :create, :edit, :show, :update, ]
   resources :events, only: [:new, :create]
