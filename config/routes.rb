aa2c3a4a06e64cf7eceaed72ddc2706da4e1fc8a
filipe dias <filipediasit@gmail.com>
2022-09-10 15:12:31 +0100

    added calendar views and added css

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   resources :events, only: [:new, :create]
   get 'stats', to: 'pages#stats'
   get 'month', to: 'calendar#month'
-  get 'week', to: 'calendar#week'
   get 'day', to: 'calendar#day'
 end
