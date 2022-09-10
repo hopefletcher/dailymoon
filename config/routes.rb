19e2cd4bd564034b590d7dab973a9df79778027a
George Chaves <george_hfc@yahoo.com.br>
2022-09-10 11:01:32 +0100

    emojis, login and regstration buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'calendar#day'
+  get 'home', to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :moods, only: [:new, :create, :edit, :show, :update, ]
   resources :events, only: [:new, :create]
