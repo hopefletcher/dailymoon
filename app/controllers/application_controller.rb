00adf5f6358d266a8354d301638146419cee59b5
George Chaves <george_hfc@yahoo.com.br>
2022-09-15 00:15:57 +0100

    fetching sun sign before user creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_theme
+  # Setting up additional sign up fields for Devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_theme
     if params[:theme].present?
@@ -11,6 +13,12 @@ class ApplicationController < ActionController::Base
     end
 
     @zodiac_emoji = zodiac_emoji
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:location, :birthday, :zodiac_sign, :first_name, :last_name])
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Fetches Moonsign right after User creation
-  after_create :fetch_moon_sign
+  before_create :fetch_moon_sign
 
   private
 
   def fetch_moon_sign
+    # First match birthday to dd-mm-yyyy (mind you the self.birthday is now of Date class)
+    stripdate = self.birthday.strftime('%d-%m-%Y')
+    date = stripdate.match(/(\d{2})-(\d{2})-(\d{4})/)
+    # Now fetch from API
     url = 'https://json.astrologyapi.com/v1/planets'
-    result = HTTParty.post(url,
-      :body => { :day => Date.today.day,
-                 :month => Date.today.month,
-                 :year => Date.today.year,
+    results = HTTParty.post(url,
+      :body => { :day => date[1],
+                 :month => date[2],
+                 :year => date[3],
+                 # :day => Date.today.day,
+                 # :month => Date.today.month,
+                 # :year => Date.today.year,
                  :min => Time.now.min,
                  :hour => Time.now.hour,
                  :tzone => 1,
@@ -24,9 +31,8 @@ class User < ApplicationRecord
                  :lon => 2.1401891
                }.to_json,
       :headers => { 'Content-Type' => 'application/json' },
-      :basic_auth => {:username => "620589", :password => "42167510aaf892ac7f9e0efd947fed78"} )
-      moon = @result.find { |result| result["name"] == "Moon"}
-      moon["sign"]
-      @moon_sign = moon["sign"]
+      :basic_auth => { :username => "620589", :password => "42167510aaf892ac7f9e0efd947fed78" })
+      zodiac = results.find { |result| result["name"] == "Sun"}
+      self.zodiac_sign = zodiac["sign"]
   end
 end
