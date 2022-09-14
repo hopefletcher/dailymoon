bce26aa692559b7fe73b6eb8aca724765d633c93
George Chaves <george_hfc@yahoo.com.br>
2022-09-14 22:14:53 +0100

    allowing for user sign up, added form fields in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Fetches Moonsign right after User creation
+  after_create :fetch_moon_sign
+
+  private
+
+  def fetch_moon_sign
+    url = 'https://json.astrologyapi.com/v1/planets'
+    result = HTTParty.post(url,
+      :body => { :day => Date.today.day,
+                 :month => Date.today.month,
+                 :year => Date.today.year,
+                 :min => Time.now.min,
+                 :hour => Time.now.hour,
+                 :tzone => 1,
+                 :lat => 41.3926679,
+                 :lon => 2.1401891
+               }.to_json,
+      :headers => { 'Content-Type' => 'application/json' },
+      :basic_auth => {:username => "620589", :password => "42167510aaf892ac7f9e0efd947fed78"} )
+      moon = @result.find { |result| result["name"] == "Moon"}
+      moon["sign"]
+      @moon_sign = moon["sign"]
+  end
 end
