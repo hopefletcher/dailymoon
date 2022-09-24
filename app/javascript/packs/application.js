c9eb5da3e62d5bcca55e398876d2ff0312e766a1
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-09-24 09:15:07 +0100

    changed button navigation layout slightly

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,4 +14,21 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
-import "chartkick/chart.js"
+// import "chartkick/chart.js"
+
+// Get the container element
+var btnContainer = document.getElementById("nav");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
+console.log(btnContainer);
+console.log(btns);
