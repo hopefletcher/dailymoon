d1103e8157091c4d81d3a6beb6c06d455ee4a96f
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-09-29 12:36:33 +0100

    another try to fix event deletion

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :events, only: [:index, :new, :create, :edit, :update, :destroy]
 
+  delete 'events', to: 'events#destroy'
   get 'stats', to: 'pages#stats'
   get 'month', to: 'calendar#month'
   get 'day', to: 'calendar#day'
